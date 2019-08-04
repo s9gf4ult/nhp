5ba843cfcd402793ae8cfb77d69617f43fcff1c4
Alexey Uimanov <s9gf4ult@gmail.com>
2019-08-04 17:33:14 +0500

    something done

--- src/NHP.hs
+++ src/NHP.hs
@@ -2,14 +2,26 @@
 
 module NHP where
 
+data Deps = Deps (Set PackageId)
+
 -- | Raw package definition without resolved paths and so on. May be
 -- overriden and changed at anly time.
-data PackageDef
+data PackageDef = PackageDef
+  { pdId    :: PackageId
+  , pdBuild :: m Package
+  }
+
+runM :: m Package -> (Package, Deps)
+runM x = undefined
 
 -- | Baked package with resolved paths and dependencies built in. Can
 -- not be changed, can be only resolved from package bucket. Have link
 -- to the original package and bucket, so you always can reresolve it.
-data Package
+data Package = Package
+  { pId         :: PackageId
+  , pDerivation :: Derivation
+  , pDeps       :: [Package]
+  }
 
 -- | Maybe just Text inside.
 data PackageId
